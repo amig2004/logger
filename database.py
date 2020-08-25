d76a9327467d7da36dc216e349d43ed4eaab1980
Andrzej Kopec <andrzej.kopec95@gmail.com>
2020-08-25 11:01:17 +0200

    database context + deleting views dir

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
 import pymysql
 from flask import current_app, g
 
-def prepare_db(dbcfg: dict):
-    print('HOST: ', dbcfg['host'])
+def prepare_db():
+    print('HOST: ', current_app.config['DATABASE']['host'])
     try:
         if 'dbhandler' not in g:
             g.dbhandler = pymysql.connect(
-                host=dbcfg['host'],
-                user=dbcfg['user'],
-                password=dbcfg['pass'],
-                database=dbcfg['dbname']
+                host=current_app.config['DATABASE']['host'],
+                user=current_app.config['DATABASE']['user'],
+                password=current_app.config['DATABASE']['pass'],
+                database=current_app.config['DATABASE']['dbname']
                 )
             g.db = dbahndler.cursor()
             print("INFO: database loaded")
@@ -19,7 +19,9 @@ def prepare_db(dbcfg: dict):
         print('DB ERROR: ', e)
         exit(-1)
 
-@app.teardown_appcontext
 def disconnect_db():
     if hasattr(g, 'db'):
         g.db.close()
+
+def init_app(app):
+    app.teardown_appcontext(disconnect_db)
